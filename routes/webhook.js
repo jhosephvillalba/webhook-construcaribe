715ecb69ce1a5645fcd995360f8e07bb40801ab6
jhosephvillalba <enriquevillalba97@outlook.com>
2024-09-09 16:53:58 -0500

    se agrego un middleware

--- routes/webhook.js
+++ routes/webhook.js
@@ -26,10 +26,38 @@ router.get('/', (req, res) => {
   });
 
   router.post('/', (req, res) => {
-
-    console.log("SE EJECUTO EL POST");
-
-    res.status(200).send('EVENT_RECEIVED');
+    let body = req.body;
+  
+    // Checks if this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // // Iterates over each entry - there may be multiple if batched
+      // body.entry.forEach(function(entry) {
+  
+      //   // Gets the body of the webhook event
+      //   let webhookEvent = entry.messaging[0];
+      //   console.log(webhookEvent);
+  
+      //   // Get the sender PSID
+      //   let senderPsid = webhookEvent.sender.id;
+      //   console.log('Sender PSID: ' + senderPsid);
+  
+      //   // Check if the event is a message or postback and
+      //   // pass the event to the appropriate handler function
+      //   if (webhookEvent.message) {
+      //     handleMessage(senderPsid, webhookEvent.message);
+      //   } else if (webhookEvent.postback) {
+      //     handlePostback(senderPsid, webhookEvent.postback);
+      //   }
+      // });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+  
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
   });
 
 module.exports = router; 
