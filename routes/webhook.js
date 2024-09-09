77e508585b204f1d87b4512b805bd76b78fb33e7
jhosephvillalba <enriquevillalba97@outlook.com>
2024-09-09 11:58:45 -0500

    first_commit

---
+++ routes/webhook.js
@@ -0,0 +1,28 @@
+const express = require('express'); 
+const router = express.Router(); 
+
+router.get('/', (req, res) => {
+    // Capturar los valores de los parámetros de consulta
+    const mode = req.query['hub.mode'];
+    const challenge = req.query['hub.challenge'];
+    const verifyToken = req.query['hub.verify_token'];
+  
+    // Imprimir los valores para verificar que los capturamos
+    console.log('Hub Mode:', mode);
+    console.log('Hub Challenge:', challenge);
+    console.log('Hub Verify Token:', verifyToken);
+  
+    // Si la verificación es exitosa, responde con el 'challenge'
+    const verify_token = process.env.VERIFY_TOKEN; // El token que has definido
+  
+    if (mode === 'subscribe' && verifyToken === verify_token) {
+      // Verificación exitosa, responde con el challenge
+      res.status(200).send(challenge);
+      
+    } else {
+      // Verificación fallida
+      res.sendStatus(403);
+    }
+  });
+
+module.exports = router; 
